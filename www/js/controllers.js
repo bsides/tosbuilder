ec858387d648592aeb5f1d25a5bbb3b93c0e3d2c
Rafael Batista Pereira <cin_rpereira@uolinc.com>
2016-05-05 18:53:59 -0300

    Initialized some data

---
+++ www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('JobsCtrl', function($scope, Jobs) {
+  $scope.jobs = Jobs.all();
+  $scope.remove = function(job) {
+    Jobs.remove(job);
+  };
+})
+
+.controller('JobDetailCtrl', function($scope, $stateParams, Jobs) {
+  $scope.job = Jobs.get($stateParams.jobId);
+})
+;
---
+++ www/js/services.js
@@ -0,0 +1,78 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+.factory('Jobs', function() {
+
+  var jobs = [{
+    id: 0,
+    name: 'Swordsman'
+  }, {
+    id: 1,
+    name: 'Archer'
+  }];
+
+  return {
+    all: function() {
+      return jobs;
+    },
+    remove: function(job) {
+      jobs.splice(jobs.indexOf(job), 1);
+    },
+    get: function(jobId) {
+      for (var i = 0; i < jobs.length; i++) {
+        if (jobs[i].id === parseInt(jobId)) {
+          return jobs[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+;
